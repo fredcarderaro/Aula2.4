db42e7132f577c93332fb55938d51ddb1f720e65
fredCarderaro <fredcarderaro@gmail.com>
2020-08-31 16:36:36 -0300

    Adiciona rota de put

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -59,4 +59,23 @@ app.delete('/student/:id', async (req, res) => {
   }
 });
 
+// UPDATE PUT
+app.put('/student/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const student = await studentModel.findByIdAndUpdate(
+      { _id: id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 export { app as studentRouter };
